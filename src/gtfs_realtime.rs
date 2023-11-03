7f734864f8b1aa00393021c587587555ae73f379
Beatrix Klebe <bklebe@mbta.com>
2023-11-02 20:35:07 -0400

    fix(rs/gtfs-realtime): make timestamp optional

--- src/gtfs_realtime.rs
+++ src/gtfs_realtime.rs
@@ -43,7 +43,7 @@ pub(crate) struct VehiclePosition {
     pub(crate) multi_carriage_details: Option<Vec<CarriageDetails>>,
     pub(crate) position: Position,
     pub(crate) stop_id: Option<String>,
-    pub(crate) timestamp: i64,
+    pub(crate) timestamp: Option<i64>,
     pub(crate) trip: Option<Trip>,
     pub(crate) vehicle: VehicleDescriptor,
 }
