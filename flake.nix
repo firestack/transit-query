057a05bbc1c2a996f2eed9fddb3e86b36678e615
Kayla Firestack <kfirestack@mbta.com>
2024-01-27 11:41:43 -0500

    fix(flake): pin MBTA GTFS

--- flake.nix
+++ flake.nix
@@ -138,9 +138,9 @@
 
 					mbta-gtfs = pkgs.fetchzip {
 						name = "MBTA GTFS Static";
-						url = "https://cdn.mbta.com/MBTA_GTFS.zip";
+						url = "https://cdn.mbtace.com/archive/20240111.zip";
 						stripRoot = false;
-						hash = "sha256-WB4q+DqB99ruj76wuU3VfBYmIZWFaBBGMaNFa6Z/QqI=";
+						hash = "sha256-QGLOPY9CVbW6BjcjBt2UmMh5tOeeozwE92p+vnZoK1o=";
 					};
 				};
 
